15081aac1c4f12e92cfc834d632afaf78a28f6b4
HackDehZack <Zackseriousemail@gmail.com>
2023-08-31 00:54:40 -0400

    getting started on the js, finished up html doc

---
+++ javascript.js
@@ -0,0 +1,8 @@
+const questions = [
+    {
+      question: "Which programming language is known as the 'language of the web'?",
+      choices: ["JavaScript", "Python", "Ruby", "Java"],
+      correctAnswerIndex: 0
+    },
+    // Add more questions here!!
+  ];
