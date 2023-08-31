f9b3cb0444cb265de92293111385ec8a77a85730
HackDehZack <Zackseriousemail@gmail.com>
2023-08-31 01:13:28 -0400

    timer added in js

--- javascript.js
+++ javascript.js
@@ -4,41 +4,7 @@ const questions = [
       choices: ["Hyper Text Markup Language", "Home Tool Markup Language", "Hyperlinks and Text Markup Language", "Hyper Text Multiple Language"],
       correctAnswerIndex: 0
     },
-    {
-      question: "Which HTML element is used to define the title of a webpage?",
-      choices: ["<header>", "<h1>", "<title>", "<section>"],
-      correctAnswerIndex: 2
-    },
-    {
-      question: "Which CSS property is used to change the text color of an element?",
-      choices: ["background-color", "font-family", "text-decoration", "color"],
-      correctAnswerIndex: 3
-    },
-    {
-      question: "What is the correct syntax for a JavaScript comment?",
-      choices: ["// This is a comment", "<!-- This is a comment -->", "/* This is a comment */", "# This is a comment"],
-      correctAnswerIndex: 0
-    },
-    {
-      question: "Which JavaScript method is used to add new elements to an array?",
-      choices: ["push()", "concat()", "pop()", "slice()"],
-      correctAnswerIndex: 0
-    },
-    {
-      question: "Which CSS property is used to create space between the border and content of an element?",
-      choices: ["margin", "padding", "border", "display"],
-      correctAnswerIndex: 1
-    },
-    {
-      question: "What does the 'DOMContentLoaded' event represent in JavaScript?",
-      choices: ["The page has finished loading", "The user has clicked on an element", "The user has submitted a form", "The DOM tree has been fully constructed"],
-      correctAnswerIndex: 3
-    },
-    {
-      question: "Which HTML element is used to include external JavaScript files?",
-      choices: ["<script>", "<link>", "<style>", "<head>"],
-      correctAnswerIndex: 0
-    }
+    // Add my remaining questions here
   ];
   
   const startButton = document.getElementById("start-btn");
@@ -47,9 +13,11 @@ const questions = [
   const choicesElement = document.getElementById("choices");
   const nextButton = document.getElementById("next-btn");
   const resultContainer = document.getElementById("result-container");
+  const timerElement = document.getElementById("timer"); // Add a timer element in my HTML
   
   let currentQuestionIndex = 0;
   let score = 0;
+  let timer;
   
   startButton.addEventListener("click", startQuiz);
   nextButton.addEventListener("click", () => {
@@ -61,6 +29,7 @@ const questions = [
     startButton.classList.add("hide");
     questionContainer.classList.remove("hide");
     showQuestion();
+    startTimer(); // Start the timer when the quiz starts
   }
   
   function showQuestion() {
@@ -86,6 +55,9 @@ const questions = [
     if (currentQuestionIndex === questions.length - 1) {
       nextButton.innerText = "Finish";
     }
+  
+    resetTimer(); // Reset the timer for each new question
+    startTimer(); // Start the timer for the current question
   }
   
   function checkAnswer(index) {
@@ -112,4 +84,24 @@ const questions = [
     resultContainer.classList.remove("hide");
     const scoreElement = document.getElementById("score");
     scoreElement.innerText = `You scored ${score} out of ${questions.length}!`;
+  }
+  
+  function startTimer() {
+    let timeLeft = 45; // Set the time limit for each question (in seconds)
+    timerElement.innerText = timeLeft;
+  
+    timer = setInterval(() => {
+      timeLeft--;
+      timerElement.innerText = timeLeft;
+  
+      if (timeLeft === 0) {
+        clearInterval(timer);
+        checkAnswer(-1); // Call checkAnswer with an invalid index to indicate time's up
+      }
+    }, 1000);
+  }
+  
+  function resetTimer() {
+    clearInterval(timer);
+    timerElement.innerText = "";
   }
